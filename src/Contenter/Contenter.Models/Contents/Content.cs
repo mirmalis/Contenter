fca1bb091db598561edb78c9e172f09cbb2c4bcc
Šarūnas <sarunas1991@gmail.com>
2024-06-30 21:14:41 +0300

    2024-06-30 21:13

--- src/Contenter/Contenter.Models/Contents/Content.cs
+++ src/Contenter/Contenter.Models/Contents/Content.cs
@@ -6,13 +6,12 @@ public class Content: IIded<Guid>
   public override string ToString() => this.Name;
   public Guid Id { get; set; }
   public DateTime CreatedAt { get; set; }
-  public DateTime? PublishedAt { get; set; }
+  public DateOnly? PublishedAt { get; set; }
 
   public ContentFam? Fam { get; set; } 
   public Guid? FamId { get; set; }
 
-  [Required]
-  public string Name { get; set; } = default!;
+  public string? Name { get; set; } = default!;
 
   public List<ContentGuests<Contenter.Models.Persons.Persona>> GuestPersonaAssignments { get; set; } = [];
   public List<ContentSources> SourceAssignments { get; set; } = [];
---
+++ src/Contenter/Contenter/Helpers.cs
@@ -0,0 +1,9 @@
+﻿namespace Contenter;
+
+static class Helpers
+{
+  public static System.Linq.Expressions.Expression<Func<string, string>> NameRetrievalExpression(int maxLength)
+  {
+    return (name) => name.Length > maxLength ? name.Substring(0, maxLength) + ".." : name;
+  }
+}
