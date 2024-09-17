25fdcfd5bdff391e375b38909a4b84f38be90f10
Šarūnas <sarunas1991@gmail.com>
2024-09-17 11:04:17 +0300

    Added: bookmarklet importavimas

---
+++ src/Contenter/Components/Pages/Js.cs
@@ -0,0 +1,17 @@
+﻿namespace Contenter.Components.Pages;
+
+internal static class Js
+{
+  public static readonly string YoutubeVideo =
+    """
+    javascript:(
+      function ()
+      {
+        const host = "https://sars.lt";
+        (async function () {
+          window.open(`${host}/create/s/sources?PlatformId=yt&Href=${document.location.href}&bookmarkletversion=1`, "_blank");
+        })();
+      }
+    )();
+    """;
+}
